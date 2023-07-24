75f6216d3029b1eacd37ec7cb808de788ecdb183
Anticubex <teofontana00@gmail.com>
2023-07-24 15:47:43 -0700

    Uncomment real code

--- main.cpp
+++ main.cpp
@@ -11,20 +11,20 @@
 int main(int argc, char *argv[]) {
 
         // Main loop
-        // SDL_State state;
+        SDL_State state;
 
-        // int initGameCode = initGame(&state, argc, argv);
+        int initGameCode = initGame(&state, argc, argv);
 
-        // if (initGameCode != 0) {
-        //         return initGameCode;
-        // }
+        if (initGameCode != 0) {
+                return initGameCode;
+        }
 
-        // if (runGame(&state) != 0) {
-        //         destroyGame(&state);
-        //         return 1;
-        // }
+        if (runGame(&state) != 0) {
+                destroyGame(&state);
+                return 1;
+        }
 
-        // destroyGame(&state);
+        destroyGame(&state);
 
         return 0;
 }
